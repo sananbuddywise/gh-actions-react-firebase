932a32e3dc10db547b3cca48f6571ee156bf5b3c
sananredworklab <sanan@redworklab.com>
2023-06-28 17:48:21 +0500

    dev branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>src/App.js</code> and save to reload.
+          This is dev branch <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
